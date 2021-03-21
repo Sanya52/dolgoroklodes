086d7a378bc2229f988394d3674f7cba9cde7a36
Sanya52 <70943490+Sanya52@users.noreply.github.com>
2021-03-21 21:51:14 +0100

    Add files via upload

---
+++ dolgozatoroklodes/dolgozatoroklodes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dolgozatoroklodes
+{
+    class Weapon
+    {
+        public int demage;
+        public string name;
+        public Weapon(int d, string n)
+        {
+            demage = d;
+            name = n;
+
+        }
+
+    }
+    class Character
+    {
+        public int hp;
+        public int lvl;
+        public int strength;
+        public int agility;
+        public int endurance;
+        public Weapon(int d,string n)
+        {
+            return Weapon;
+
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.ReadKey();
+        }
+    }
+}
